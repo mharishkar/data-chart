6057a71652fd46e25119cafd592519267b6f81ab
Harishkar M <3harish.m@gmail.com>
2019-07-25 12:35:18 +0530

    Update app.component.service.ts

--- src/app/app.component.service.ts
+++ src/app/app.component.service.ts
@@ -8,8 +8,8 @@ export class AppService {
     private http: HttpClient
   ) { }
 
-  // url = 'https://honoredfortunateprogrammers--five-nine.repl.co';
-  url = 'http://localhost:8077/get-random-number';
+   url = 'https://honoredfortunateprogrammers--vigneshwaran3.repl.co'; // for temporary to host
+ // url = 'http://localhost:8077/get-random-number';
 
   getRandomNumber(params) {
     return this.http.get(`${this.url}`, {params, observe: 'response'});
